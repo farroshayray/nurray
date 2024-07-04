1f64d8d8d42456c04d80dd07184415b03ec6194c
farroshayray <farros.hr@gmail.com>
2024-07-04 19:46:07 +0700

    add responsive carousel images

--- scripts/index.js
+++ scripts/index.js
@@ -40,3 +40,41 @@
         function closeContactForm() {
             document.getElementById('contactFormPopup').style.display = 'none';
         }
+
+    
+
+        // slideshow
+    let slideIndex = 1;
+    showSlides(slideIndex);
+
+    function plusSlides(n) {
+    showSlides(slideIndex += n);
+    }
+
+    function currentSlide(n) {
+    showSlides(slideIndex = n);
+    }
+
+    function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    }
+
+
+    // Automatic slideshow every 4 seconds
+    setInterval(function() {
+        slideIndex++;
+        if (slideIndex > slides.length) { slideIndex = 1; }
+        showSlides(slideIndex);
+    }, 1000);
